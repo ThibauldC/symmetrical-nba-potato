12e6e877492a3306df1cb5f36489dad6cbd3940e
ThibauldC <croonenborghsthibauld@gmail.com>
2025-10-23 16:00:49 +0200

    feat: fallback when box score returns null for points and record

--- nba.py
+++ nba.py
@@ -7,7 +7,7 @@ import sys
 import requests
 
 logging.basicConfig(
-    filename="tmp.txt",
+    filename="logs/nba.txt",
     filemode="a",
     format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -40,10 +40,12 @@ class Game:
     away_team: Team
 
     def __str__(self):
-        return (f"{self.home_team.abbreviation}({self.home_team.record})-{self.away_team.abbreviation}({self.away_team.record}): {self.home_team.pts}-{self.away_team.pts}")
+        home_team_record = f"({self.home_team.record})" if self.home_team.record is not None else ""
+        away_team_record = f"({self.away_team.record})" if self.away_team.record is not None else ""
+        return f"{self.home_team.abbreviation}{home_team_record}-{self.away_team.abbreviation}{away_team_record}: {self.home_team.pts}-{self.away_team.pts}"
 
 
-def get_game_info(game_id: str) -> Game:
+def get_game_info(game_id: str, team_scores: dict[str, int]) -> Game:
     game_url = "https://stats.nba.com/stats/boxscoresummaryv2"
     params = {
         "GameID": game_id
@@ -55,16 +57,19 @@ def get_game_info(game_id: str) -> Game:
         if resp.status_code != 200:
             LOGGER.error(f"No game info found for id {game_id}, status code {resp.status_code}")
     all_game_info = [info["rowSet"] for info in resp.json().get("resultSets") if info["name"] == "LineScore"][0]
+    # TODO: get home and away team correct
     home_game_stats, away_game_stats = all_game_info
-    print(home_game_stats, away_game_stats)
+    home_team_abbr, away_team_abbr = home_game_stats[4], away_game_stats[4]
+    home_team_pts = home_game_stats[-1] if home_game_stats[-1] is not None else team_scores[home_team_abbr]
+    away_team_pts = away_game_stats[-1] if away_game_stats[-1] is not None else team_scores[away_team_abbr]
 
-    home_team = Team(home_game_stats[4], home_game_stats[-1], home_game_stats[7])
-    away_team = Team(away_game_stats[4], away_game_stats[-1], away_game_stats[7])
+    home_team = Team(home_team_abbr, home_team_pts, home_game_stats[7])
+    away_team = Team(away_team_abbr, away_team_pts, away_game_stats[7])
 
     return Game(home_team, away_team)
 
 
-def get_last_nights_games(date: datetime) -> tuple[set[str], dict[str, str]] | None:
+def get_last_nights_games(date: datetime) -> tuple[set[str], dict[str, int]] | None:
     params = {
         "playerOrTeam": "T",
         "LeagueID": "00",
@@ -92,7 +97,5 @@ if __name__ == "__main__":
         LOGGER.info("No games found last night")
         sys.exit(0)
 
-    #for game_id in games:
-        #print(get_game_info(game_id))
-
-    LOGGER.info(f"{len(games)} games found")
+    for game_id in games:
+        print(get_game_info(game_id, team_scores))
